b4a3de3a973c4e48274f1f9eedbcba86547200a0
manemejeff <kmb14056@yandex.ru>
2020-02-27 23:02:16 +0400

    Added includes p.7

---
+++ README.md
@@ -0,0 +1,1 @@
+I`ll try to put here usefull information I swear.
--- app_engine/main/views.py
+++ app_engine/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 
@@ -18,11 +19,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
     form = UserCreationForm
     return render(request=request,
