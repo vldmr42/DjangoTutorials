b934a89783ef0343a8b87a40ad0f508dc6c2fea3
manemejeff <kmb14056@yandex.ru>
2020-03-01 19:39:22 +0400

    Added tutorial.html and sidebar p.11

--- app_engine/app_engine/urls.py
+++ app_engine/app_engine/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls')),
     path('admin', admin.site.urls),
+    path('', include('main.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
+
